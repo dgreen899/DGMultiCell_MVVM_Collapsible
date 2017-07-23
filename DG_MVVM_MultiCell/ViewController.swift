6f63d662e562caf6c4b52c480d73c3f48ff8ac6c
Dameon <dameongreen@gmail.com>
2017-07-23 16:58:58 -0400

    Initial Commit

---
+++ DG_MVVM_MultiCell/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DG_MVVM_MultiCell
+//
+//  Created by Dameon Green on 7/23/17.
+//  Copyright © 2017 ApptasticVoyage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
